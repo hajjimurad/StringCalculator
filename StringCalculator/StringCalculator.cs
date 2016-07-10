229fb588457018970ebe430923452c45409732a0
murad <hajjimurad@gmail.com>
2016-07-10 11:28:50 +0200

    initial tests

---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace StringCalculator
+{
+    class StringCalculator
+    {
+        internal int Add(string source)
+        {
+            if (source == "")
+                return 0;
+
+            var numbersRaw = source.Split(',');            
+            var numbersInt = numbersRaw.Select(item => int.Parse(item));
+
+            return numbersInt.Aggregate((sum, item) => item + sum);
+        }
+    }
+}
---
+++ StringCalculator/StringCalculatorTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculator
+{
+    [TestFixture]
+    public class StringCalculatorTest
+    {
+        private static StringCalculator GetStringClaculator()
+        {
+            return new StringCalculator();
+        }
+
+        [Test]
+        public void Add_EmptyString_ReturnsZero()
+        {
+            StringCalculator sc = GetStringClaculator();
+            int result = sc.Add("");
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        [TestCase("3", 3)]
+        public void Add_OneNumber_ReturnsNumber(string number, int expected)
+        {
+            StringCalculator sc = GetStringClaculator();
+            int result = sc.Add(number);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("1,2", 3)]
+        [TestCase("1,2,3", 6)]
+        [TestCase("1,2,3,4", 10)]
+        public void Add_TwoNumbersDelimited_ReturnsSum(string numbers, int expected)
+        {
+            StringCalculator sc = GetStringClaculator();
+            int result = sc.Add(numbers);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
